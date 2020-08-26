84a1a1b8ccc586ede1268bb8ca3c8de54e1d4e22
Ryan Kelch <ryannkelch@gmail.com>
2020-08-25 20:55:00 -0700

    Added Clicked Int message

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (..)
+module Main exposing (main)
 
 -- Press buttons to increment and decrement a counter.
 --
@@ -6,10 +6,10 @@ module Main exposing (..)
 --   https://guide.elm-lang.org/architecture/buttons.html
 --
 
-
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes as Attr
+import Html.Events as Events
 
 
 
@@ -17,19 +17,24 @@ import Html.Events exposing (onClick)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 
 -- MODEL
 
 
-type alias Model = Int
+type alias Model =
+    Int
 
 
 init : Model
 init =
-  0
+    0
 
 
 
@@ -37,18 +42,14 @@ init =
 
 
 type Msg
-  = Increment
-  | Decrement
+    = Clicked Int
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
+    case msg of
+        Clicked num ->
+            model
 
 
 
@@ -57,8 +58,34 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ div []
+            [ input [] []
+            , button [] [ text "calc" ]
+            ]
+        , div []
+            (List.range 1 3
+                |> List.map viewNumberButton
+            )
+        , div []
+            (List.range 4 6
+                |> List.map viewNumberButton
+            )
+        , div []
+            (List.range 7 9
+                |> List.map viewNumberButton
+            )
+
+        -- [ button [] [ text "1" ], button [] [ text "2" ], button [] [ text "3" ] ]
+        -- , div []
+        --     [ button [] [ text "4" ], button [] [ text "5" ], button [] [ text "6" ] ]
+        -- , div []
+        --     [ button [] [ text "7" ], button [] [ text "8" ], button [] [ text "9" ] ]
+        , div []
+            [ viewNumberButton 0, button [] [ text "+" ], button [] [ text "-" ] ]
+        ]
+
+
+viewNumberButton : Int -> Html Msg
+viewNumberButton num =
+    button [ Events.onClick (Clicked num) ] [ text (String.fromInt num) ]
